e493fe9766a53ff919e4a0b890621eda42c39648
regule <regule@zoho.com>
2020-12-27 16:40:36 +0100

    Signal generation works

--- python/sigproc/generate_signal.py
+++ python/sigproc/generate_signal.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 PROGRAM_DESCRIPTION = 'This program is used for generation of signals (WRITE MORE)'
 
@@ -12,6 +13,9 @@ def generate_sampling(start_timestamp, end_timestamp, sampling_frequency):
 def randomize(lims):
     return np.random.uniform(lims[0], lims[1])
 
+def parse_range(range_str):
+    lower, upper = map(float, range_str.split(','))
+    return (lower, upper)
 
 class Sinusoid:
 
@@ -39,17 +43,53 @@ class SignalGenerator:
         self.sinusoids = []
 
     def __getitem__(self, index):
-         return self.sinusoids[index]
+        return self.sinusoids[index]
 
+    def append(self, sinusoid):
+        self.sinusoids.append(sinusoid)
+
+    def generate_signal(self, sampling):
+        signal = self.sinusoids[0].generate_signal(sampling)
+        for sinusoid in self.sinusoids[1:]:
+            signal += sinusoid.generate_signal(sampling)
+        return signal
+
+    @staticmethod
+    def generate_random(sinusoid_count, amplitude, frequency, phase, offset):
+        generator = SignalGenerator()
+        for _ in range(sinusoid_count):
+            generator.append(Sinusoid.generate_random(amplitude, frequency, phase, offset))
+        return generator
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
+    parser.add_argument('-c', '--csv_file',
+            default=None, help='File to which signal data will be saved')
+    parser.add_argument('-s', '--sin_count', default=1, type=int,
+            help='Number of sinusoids')
+    parser.add_argument('-a', '--amplitude', default=(1,10), type=parse_range,
+            help='Number of sinusoids')
+    parser.add_argument('-f', '--frequency', default=(1,1000), type=parse_range,
+            help='Number of sinusoids')
+    parser.add_argument('-p', '--phase', default=(0,0), type=parse_range,
+            help='Number of sinusoids')
+    parser.add_argument('-o', '--offset', default=(0,0), type=parse_range,
+            help='Number of sinusoids')
     return parser.parse_args()
 
 
 def main(args):
-    sinusoid = Sinusoid.generate_random([1.0, 10.0], [1.0, 10000.0], [0.0, 3.0], [0.0, 2.0])
+    sinusoid = SignalGenerator.generate_random(args.sin_count, args.amplitude,
+            args.frequency, args.phase, args.offset)
     print(f'{sinusoid}')
+    sampling = generate_sampling(0,10,100)
+    signal = sinusoid.generate_signal(sampling)
+    plt.plot(sampling, signal)
+    plt.show()
+    if args.csv_file is not None:
+        data = {'timestamp':sampling, 'signal':signal}
+        data = pd.DataFrame(data)
+        data.to_csv(args.csv_file, index=False)
 
 
 if __name__ == '__main__':
