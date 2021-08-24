1163cd19bdcdcf500949b033fac968c3f54b390e
regule <regule@runbox.com>
2021-08-24 21:40:17 +0200

    Now this thing works

---
+++ rust/pong-zrzynka/src/main.rs
@@ -0,0 +1,37 @@
+use ggez;
+use ggez::{graphics, event};
+use ggez::{GameResult, GameError, ContextBuilder, Context};
+use ggez::graphics::{Color};
+use ggez::event::EventHandler;
+
+fn main() -> GameResult {
+    let (mut context, event_loop) = ContextBuilder::new("Zrzynka", "Kolektyw").build()?;
+    graphics::set_window_title(&context, "Zrzynka");
+    let mut game = PongGame::new();
+    event::run(context, event_loop, game);
+    Ok(())
+}
+
+
+struct PongGame{
+}
+
+
+impl PongGame{
+    pub fn new() -> PongGame{
+        PongGame{}
+    }
+}
+
+impl EventHandler<GameError> for PongGame{
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Update code here...
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        graphics::clear(ctx, Color::WHITE);
+        // Draw code here...
+        graphics::present(ctx)
+    }
+}
