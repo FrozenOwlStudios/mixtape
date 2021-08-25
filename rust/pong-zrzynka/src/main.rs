eead95b8d988e6167261bd4f5077e9ca6cd9c045
regule <regule@runbox.com>
2021-08-25 19:29:46 +0200

    It draws

--- rust/pong-zrzynka/src/main.rs
+++ rust/pong-zrzynka/src/main.rs
@@ -3,6 +3,14 @@ use ggez::{graphics, event};
 use ggez::{GameResult, GameError, ContextBuilder, Context};
 use ggez::graphics::{Color};
 use ggez::event::EventHandler;
+use ggez::mint::Vector2;
+
+
+const RACKET_HEIGHT:f32 = 100.0;
+const RACKET_WIDTH:f32 = 20.0;
+const RACKET_HEIGHT_HALF:f32 = RACKET_HEIGHT*0.5;
+const RACKET_WIDTH_HALF:f32 = RACKET_WIDTH*0.5;
+
 
 fn main() -> GameResult {
     let (mut context, event_loop) = ContextBuilder::new("Zrzynka", "Kolektyw").build()?;
@@ -12,25 +20,63 @@ fn main() -> GameResult {
 }
 
 
+
+struct Racket{
+    position: Vector2<f32>,
+    size: Vector2<f32>,
+    color: Color,
+}
+
+impl Racket{
+    pub fn new(x: f32, y: f32, w: f32, h: f32, color: Color) -> Racket{
+        Racket{
+            position: Vector2{x, y},
+            size: Vector2{x:w, y:h},
+            color
+        }
+    }
+
+    pub fn get_mesh(&self, context: &mut Context)-> graphics::Mesh{
+        let rect = graphics::Rect::new(self.position.x,
+                                       self.position.y,
+                                       self.size.x,
+                                       self.size.y);
+        graphics::Mesh::new_rectangle(context,graphics::DrawMode::fill(),
+                                      rect,self.color).unwrap()
+        
+    }
+}
+
+
 struct PongGame{
+    left_player: Racket,
 }
 
 
 impl PongGame{
     pub fn new() -> PongGame{
-        PongGame{}
+        PongGame{
+            left_player: Racket::new(RACKET_WIDTH_HALF, RACKET_HEIGHT_HALF,
+                                     RACKET_WIDTH, RACKET_HEIGHT,
+                                     Color::YELLOW)
+        }
     }
 }
 
 impl EventHandler<GameError> for PongGame{
-    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        // Update code here...
+    fn update(&mut self, context: &mut Context) -> Result<(),GameError> {
         Ok(())
     }
 
-    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
-        graphics::clear(ctx, Color::WHITE);
-        // Draw code here...
-        graphics::present(ctx)
+    fn draw(&mut self, context: &mut Context) -> Result<(),GameError> {
+        graphics::clear(context, Color::BLACK);
+        //let rect = graphics::Rect::new(100.0, 100.0, 20.0, 20.0);
+        //let mesh = graphics::Mesh::new_rectangle(context,
+        //                                         graphics::DrawMode::fill(),
+        //                                         rect,
+        //                                         Color::YELLOW).unwrap();
+        let mesh = self.left_player.get_mesh(context);
+        graphics::draw(context, &mesh, graphics::DrawParam::default());
+        graphics::present(context)
     }
 }
