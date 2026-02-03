d6ffc1d07f92c28c2b0cc79bbbf6c95bfb3487d3
FrozenOwl <regule@runbox.com>
2026-02-03 04:20:18 +0100

    Updated nvim config to one that I now use

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,120 +1,247 @@
+-- ============================================================
+-- Simple Neovim config
+-- Author: FrozenOwl
+-- ============================================================
+
+-- Leader key
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Basic options
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.mouse = "a"
+vim.opt.termguicolors = true
+vim.opt.updatetime = 200
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
+-- Indentation (good defaults for Python)
 vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.smartindent = true
+
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.termguicolors = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.cursorline = true
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80,100,120"
 
--- Bootstrap lazy.nvim plugin manager
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Better splits
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Setting colorscheme
+vim.cmd("colorscheme slate")
+
+-- ============================================================
+-- Keymaps (multi-file workflow)
+-- ============================================================
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- Save/quit
+map("n", "<leader>w", "<cmd>w<cr>", opts)
+map("n", "<leader>q", "<cmd>q<cr>", opts)
+
+-- Buffer navigation (like "tabs" for files)
+map("n", "<S-l>", "<cmd>bnext<cr>", opts)     -- Next buffer
+map("n", "<S-h>", "<cmd>bprevious<cr>", opts) -- Previous buffer
+map("n", "<leader>bd", "<cmd>bdelete<cr>", opts) -- Close buffer
+
+-- Optional: show buffers list
+map("n", "<leader>bl", "<cmd>ls<cr>", opts)
+
+-- ============================================================
+-- Bootstrap lazy.nvim (plugin manager)
+-- ============================================================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git", "clone", "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
+    "--branch=stable",
+    lazypath
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- ============================================================
+-- Plugins
+-- ============================================================
 require("lazy").setup({
-  -- Essentials
-  { "nvim-lua/plenary.nvim" },
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-  { "nvim-lualine/lualine.nvim" },
-  { "nvim-tree/nvim-tree.lua" },
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  -- File picker / search
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 
-  -- LSP + Completion
+  -- Nice "tabs" (buffer line)
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("bufferline").setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          separator_style = "slant",
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+        }
+      })
+    end
+  },
+
+  -- LSP
   { "neovim/nvim-lspconfig" },
-  { "williamboman/mason.nvim" },
-  { "williamboman/mason-lspconfig.nvim" },
+
+  -- Completion engine (manual trigger only)
   { "hrsh7th/nvim-cmp" },
   { "hrsh7th/cmp-nvim-lsp" },
-  { "hrsh7th/cmp-buffer" },
-  { "L3MON4D3/LuaSnip" },
-  { "saadparwaiz1/cmp_luasnip" },
+  { "L3MON4D3/LuaSnip" }, -- optional snippets, kept minimal
 
-  -- Language-specific
-  { "Vimjas/vim-python-pep8-indent" },
-  { "psf/black", branch = "stable" },
-
-  -- Git
-  { "lewis6991/gitsigns.nvim" },
-
-  -- Theme
-  { "gruvbox-community/gruvbox" },
-  {"lmintmate/blue-mood-vim"},
-})
-
--- Theme
-vim.cmd("colorscheme gruvbox")
-
--- Treesitter setup
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "python", "go", "lua", "json", "yaml" },
-  highlight = { enable = true },
-  indent = { enable = true },
-})
-
--- Lualine setup
-require("lualine").setup({ options = { theme = "gruvbox" } })
-
--- Nvim-tree setup
-require("nvim-tree").setup()
-
--- Gitsigns setup
-require("gitsigns").setup()
-
--- Mason setup
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "pyright",},
- })
-vim.lsp.config('*', {ensure_installed = { "pyright",}})
-
--- LSP setup
--- local lspconfig = require("lspconfig")
--- lspconfig.pyright.setup({})
--- lspconfig.gopls.setup({})
-
--- Autocomplete setup
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args) require("luasnip").lsp_expand(args.body) end,
+  -- Treesitter (better highlighting)
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
   },
-  mapping = cmp.mapping.preset.insert({
+
+  -- Formatting (optional but useful)
+  { "stevearc/conform.nvim" },
+
+  -- Terminal support
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    keys = {
+      { "<leader>tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+    },
+    opts = {
+      open_mapping = nil,      -- we bind ourselves (Normal mode)
+      direction = "float",
+      shade_terminals = false, -- keep minimal
+      float_opts = {
+        border = "rounded",
+      },
+    },
+  },
+})
+
+-- ============================================================
+-- Telescope keymaps
+-- ============================================================
+local telescope = require("telescope.builtin")
+map("n", "<leader>ff", telescope.find_files, opts) -- Find files
+map("n", "<leader>fg", telescope.live_grep, opts)  -- Grep in project
+map("n", "<leader>fb", telescope.buffers, opts)    -- Switch open files
+map("n", "<leader>fh", telescope.help_tags, opts)
+
+
+-- ============================================================
+-- LSP: Pyright
+-- ============================================================
+
+-- LSP keymaps (all manual)
+map("n", "gd", vim.lsp.buf.definition, opts)
+map("n", "gr", vim.lsp.buf.references, opts)
+map("n", "K", vim.lsp.buf.hover, opts)             -- hover docs (manual)
+map("n", "<leader>rn", vim.lsp.buf.rename, opts)
+map("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+map("n", "<leader>e", vim.diagnostic.open_float, opts)
+map("n", "[d", vim.diagnostic.goto_prev, opts)
+map("n", "]d", vim.diagnostic.goto_next, opts)
+
+-- ============================================================
+-- nvim-cmp (manual completion ONLY)
+-- ============================================================
+local cmp = require("cmp")
+
+cmp.setup({
+  completion = {
+    autocomplete = false, -- no automatic popup
+  },
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    -- Manual trigger
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  }),
+
+    -- Confirm ONLY if menu is visible
+    ["<CR>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.confirm({ select = false })
+      else
+        fallback() -- normal Enter behavior
+      end
+    end, { "i", "s" }),
+
+    -- Navigation only when menu is open
+    ["<C-n>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<C-p>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    -- Close menu explicitly
+    ["<Esc>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.abort()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
   sources = {
     { name = "nvim_lsp" },
-    { name = "buffer" },
   },
 })
 
--- Keymaps
-vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files" })
-vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Live grep" })
-vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find buffers" })
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "File explorer" })
-
-for i = 1, 9 do
-  vim.keymap.set("n", "<leader>" .. i, i .. "gt", { noremap = true, silent = true })
-end
-
--- Autoformat on save (Python, Go)
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.py",
-  command = "silent! execute ':Black'",
+-- ============================================================
+-- Formatting with conform.nvim (manual trigger)
+-- ============================================================
+require("conform").setup({
+  formatters_by_ft = {
+    python = { "black" },
+  },
 })
 
+map("n", "<leader>f", function()
+  require("conform").format({ lsp_fallback = true })
+end, opts)
+
+-- ============================================================
+-- Bufferline keymaps (more intuitive "tabs")
+-- ============================================================
+map("n", "<leader>1", "<cmd>BufferLineGoToBuffer 1<cr>", opts)
+map("n", "<leader>2", "<cmd>BufferLineGoToBuffer 2<cr>", opts)
+map("n", "<leader>3", "<cmd>BufferLineGoToBuffer 3<cr>", opts)
+map("n", "<leader>4", "<cmd>BufferLineGoToBuffer 4<cr>", opts)
+map("n", "<leader>5", "<cmd>BufferLineGoToBuffer 5<cr>", opts)
+
+
+-- ============================================================
+-- Extra config for terminal
+-- ============================================================
+vim.api.nvim_create_autocmd("TermOpen", {
+  pattern = "term://*",
+  callback = function()
+    vim.cmd("startinsert")
+    vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { buffer = true })
+  end,
+})
