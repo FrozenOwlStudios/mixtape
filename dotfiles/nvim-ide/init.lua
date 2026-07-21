4f8836becceffa354f373858e3b87d2a8287b46f
Frozen Owl <franz.blenter@gmail.com>
2026-07-21 11:49:41 +0200

    Updated nvim coloroscheme

--- dotfiles/nvim-ide/init.lua
+++ dotfiles/nvim-ide/init.lua
@@ -1,8 +1,10 @@
 -- Requires Neovim >= 0.10, git, ripgrep, fd, node/npm, uv, ruff, mypy
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require('frozen_owl.options')
-require('frozen_owl.keymaps')
-require('frozen_owl.lazy')
+require("frozen_owl.options")
+require("frozen_owl.keymaps")
+require("frozen_owl.lazy")
+
+vim.cmd.colorscheme("blue")
--- dotfiles/nvim-ide/lua/frozen_owl/plugins/core.lua
+++ dotfiles/nvim-ide/lua/frozen_owl/plugins/core.lua
@@ -1,24 +1,27 @@
 return {
-  { 'folke/which-key.nvim', event = 'VeryLazy', opts = {} },
-  { 'nvim-lua/plenary.nvim' },
+	{ "folke/which-key.nvim", event = "VeryLazy", opts = {} },
+	{ "nvim-lua/plenary.nvim" },
+	--[[
+  --Colorscheme, I will probably delete this later on
   { 'folke/tokyonight.nvim', priority = 1000, config = function()
       vim.cmd.colorscheme('tokyonight-night')
     end
   },
-  { 'nvim-tree/nvim-web-devicons' },
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    branch = 'v3.x',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim' },
-    opts = { filesystem = { filtered_items = { visible = true, hide_dotfiles = false } } },
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { defaults = { file_ignore_patterns = { '.venv/', 'node_modules/', '.git/' } } },
-  },
-  { 'akinsho/toggleterm.nvim', version = '*', opts = { open_mapping = [[<c-\>]], direction = 'float' } },
-  { 'kdheepak/lazygit.nvim', cmd = 'LazyGit', dependencies = { 'nvim-lua/plenary.nvim' } },
-  { 'folke/trouble.nvim', opts = {} },
+  ]]
+	{ "nvim-tree/nvim-web-devicons" },
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+		opts = { filesystem = { filtered_items = { visible = true, hide_dotfiles = false } } },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { defaults = { file_ignore_patterns = { ".venv/", "node_modules/", ".git/" } } },
+	},
+	{ "akinsho/toggleterm.nvim", version = "*", opts = { open_mapping = [[<c-\>]], direction = "float" } },
+	{ "kdheepak/lazygit.nvim", cmd = "LazyGit", dependencies = { "nvim-lua/plenary.nvim" } },
+	{ "folke/trouble.nvim", opts = {} },
 }
